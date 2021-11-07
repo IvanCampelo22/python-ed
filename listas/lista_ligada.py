09d329bdeeee40130d814da620af9765fc5e7c2a
IvanCampelo22 <ivanilsondasilva1973@gmail.com>
2021-11-07 13:30:32 -0300

    recuperando elemento do no

--- listas/lista_ligada.py
+++ listas/lista_ligada.py
@@ -1,7 +1,7 @@
 from no import No
 
 
-class ListaLigada():
+class ListaLigada:
     def __init__(self):
         self.__primeiro_no = None
         self.__ultimo_no = None
@@ -12,6 +12,34 @@ class ListaLigada():
         if self.esta_vazia():
             self.__primeiro_no = novo_no
             self.__ultimo_no = novo_no
+        else:
+            self.__ultimo_no.proximo = novo_no
+            self.__ultimo_no = novo_no
+        self.__tamanho +=1
 
     def esta_vazia(self):
         return self.__tamanho == 0
+
+    def __str__(self):
+        temp = self.__primeiro_no
+        elementos = ''
+        while temp:
+            elementos = f'{elementos} {temp.elemento}'
+            temp = temp.proximo
+        return elementos
+
+    def recuperar_no_elemento(self, posicao):
+        no = self.recuperar_no(posicao)
+        if no != None:
+            return no.elemento
+        return None
+
+    def recuperar_no(self, posicao):
+        resultado = 0
+        for i in range(posicao + 1):
+            if i == 0:
+                resultado = self.__primeiro_no
+            else:
+                resultado = resultado.proximo
+        return resultado
+
